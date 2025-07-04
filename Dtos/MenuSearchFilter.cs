4c7919d8531ec9e0445f988dbb3a2b709ff5f018
Thinhnthe172159 <101821797+Thinhnthe172159@users.noreply.github.com>
2025-07-04 23:48:53 +0700

    add search

---
+++ Dtos/MenuSearchFilter.cs
@@ -0,0 +1,11 @@
+﻿namespace FoodOrder.Dtos
+{
+    public class MenuSearchFilter
+    {
+        public string? Keyword { get; set; }
+        public int? CategoryId { get; set; }
+        public bool? IsAvailable { get; set; }
+        public decimal? MinPrice { get; set; }
+        public decimal? MaxPrice { get; set; }
+    }
+}
--- Services/IMenuService.cs
+++ Services/IMenuService.cs
@@ -1,4 +1,5 @@
-﻿using FoodOrder.Models;
+﻿using FoodOrder.Dtos;
+using FoodOrder.Models;
 using FoodOrderApp.Application.DTOs;
 
 namespace FoodOrder.Services
@@ -6,6 +7,7 @@ namespace FoodOrder.Services
     public interface IMenuService
     {
         Task<IEnumerable<MenuItemDto>> GetMenuItemsAsync();
+        Task<IEnumerable<MenuItemDto>> SearchMenuItemsAsync(MenuSearchFilter filter);
         Task<MenuItemDto> GetMenuItemByIdAsync(int id);
         Task<MenuItemDto> CreateMenuItemAsync(MenuItemDto dto);
         Task<MenuItemDto> UpdateMenuItemAsync(int id, MenuItemDto dto);
