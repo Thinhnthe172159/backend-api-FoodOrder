c32d16e544ddd4035b6f084e37fa2d2c18850657
HieuNonTay <hieubuivan646@gmail.com>
2025-07-07 10:34:53 +0700

    table service

---
+++ FoodOrder/Controllers/TableController.cs
@@ -0,0 +1,68 @@
+﻿using FoodOrder.IServices;
+using FoodOrder.Services;
+using FoodOrderApp.Application.DTOs;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FoodOrder.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class TableController : Controller
+{
+    private readonly ITableService _tableService;
+
+    public TableController(ITableService tableService)
+    {
+        _tableService = tableService;
+    }
+
+    // POST: api/table
+    [HttpPost]
+    public async Task<ActionResult<TableDto>> CreateTable([FromBody] TableDto dto)
+    {
+        var result = await _tableService.CreateTableAsync(dto);
+        return CreatedAtAction(nameof(GetById), new { id = result.Id }, result);
+    }
+
+    // GET: api/table
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<TableDto>>> GetAll()
+    {
+        var tables = await _tableService.GetTablesAsync();
+        return Ok(tables);
+    }
+
+    // GET: api/table/5
+    [HttpGet("{id}")]
+    public async Task<ActionResult<TableDto>> GetById(int id)
+    {
+        var table = await _tableService.GetTableByIdAsync(id);
+        if (table == null)
+            return NotFound();
+
+        return Ok(table);
+    }
+
+    // GET: api/table/qrcode/{code}
+    [HttpGet("qrcode/{code}")]
+    public async Task<ActionResult<TableDto>> GetByQrCode(string code)
+    {
+        var table = await _tableService.GetTableByQrCode(code);
+        if (table == null)
+            return NotFound();
+
+        return Ok(table);
+    }
+
+    // PUT: api/table/5/status
+    [HttpPut("{id}/status")]
+    public async Task<IActionResult> UpdateStatus(int id, [FromBody] TableStatusUpdateDto dto)
+    {
+        var success = await _tableService.UpdateTableStatusAsync(id, dto);
+        if (!success)
+            return NotFound();
+
+        return NoContent();
+    }
+
+}
---
+++ FoodOrder/Services/TableService.cs
@@ -0,0 +1,86 @@
+﻿using FoodOrder.IRepositories;
+using FoodOrder.IServices;
+using FoodOrder.Models;
+using FoodOrderApp.Application.DTOs;
+using Microsoft.EntityFrameworkCore;
+
+namespace FoodOrder.Services;
+
+public class TableService : ITableService
+{
+    private readonly FoodOrderDbContext _context;
+    private readonly ITableRepository tableRepository;
+    public TableService(FoodOrderDbContext context, ITableRepository tableRepository)
+    {
+        _context = context;
+        this.tableRepository = tableRepository;
+    }
+
+    public async Task<TableDto> CreateTableAsync(TableDto dto)
+    {
+        var table = new Table
+        {
+            TableNumber = dto.TableNumber,
+            Qrcode = dto.QRCode,
+            Status = dto.Status
+        };
+
+        _context.Tables.Add(table);
+        await _context.SaveChangesAsync();
+
+        dto.Id = table.Id;
+        return dto;
+    }
+
+    public async Task<IEnumerable<TableDto>> GetTablesAsync()
+    {
+        return await _context.Tables
+            .Select(t => new TableDto
+            {
+                Id = t.Id,
+                TableNumber = t.TableNumber,
+                QRCode = t.Qrcode,
+                Status = t.Status
+            })
+            .ToListAsync();
+    }
+
+    public async Task<TableDto> GetTableByIdAsync(int id)
+    {
+        var table = await _context.Tables.FindAsync(id);
+        if (table == null) return null;
+
+        return new TableDto
+        {
+            Id = table.Id,
+            TableNumber = table.TableNumber,
+            QRCode = table.Qrcode,
+            Status = table.Status
+        };
+    }
+
+    public async Task<TableDto> GetTableByQrCode(string code)
+    {
+        var table = tableRepository.GetTableByQrCode(code);
+
+        if (table == null) return null;
+
+        return new TableDto
+        {
+            Id = table.Result.Id,
+            TableNumber = table.Result.TableNumber,
+            QRCode = table.Result.Qrcode,
+            Status = table.Result.Status
+        };
+    }
+
+    public async Task<bool> UpdateTableStatusAsync(int id, TableStatusUpdateDto dto)
+    {
+        var table = await _context.Tables.FindAsync(id);
+        if (table == null) return false;
+
+        table.Status = dto.Status;
+        await _context.SaveChangesAsync();
+        return true;
+    }
+}
